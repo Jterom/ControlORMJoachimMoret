dc404542285bc7ae022e12144ff4dd13b01ae291
Jterom <moret4419044@gmail.com>
2019-11-14 14:50:22 +0100

    test

--- JoachimControlORM.ORM/ContextBDD.cs
+++ JoachimControlORM.ORM/ContextBDD.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace JoachimControlORM.ORM
 {
-    public class Class1
+    public class ContextBDD : DbContext
     {
+        public ContextBDD()
+            : base("ChaineDeConnexion")
+        {
+
+        }
     }
 }
